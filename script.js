e487552fd2e5739fef55f6e81d176850bf5ae432
Bradley Eversley <16165703+BradleyEversley@users.noreply.github.com>
2024-06-02 01:30:28 -1000

    added score result and update elements to new button UI

--- script.js
+++ script.js
@@ -2,16 +2,40 @@
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
+const resetButton = document.getElementById('reset');
 
 rockButton.addEventListener('click', () => playRound('Rock'));
 paperButton.addEventListener('click', () => playRound('Paper'));
 scissorsButton.addEventListener('click', () => playRound('Scissors'));
+resetButton.addEventListener('click',  resetGame);
 
 // Function to get the computer's choice
 function getCompChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const randIndex = Math.floor(Math.random() * 3);
     return choices[randIndex];
+}
+
+// Elements to update the score and result
+const scoreDisplay = document.getElementById('score');
+const resultDisplay = document.getElementById('result');
+
+let userScore = 0;
+let compScore = 0;
+const winningScore = 5;
+
+function displayWinner() {
+    if (userScore == winningScore) {
+            resultDisplay.textContent = "Congratulations! You won the game!";
+    } else {
+        resultDisplay.textContent = "Computer has won the game";
+    }
+
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+    resetButton.style.display = 'block';
+    
 }
 
 // Function to play a single round
@@ -27,12 +51,33 @@ function playRound(userChoice) {
         (userChoice === 'Scissors' && compChoice === 'Paper')
     ) {
         result = `You win! ${userChoice} beats ${compChoice}`;
+        userScore ++;
     } else {
         result = `You lose! ${compChoice} beats ${userChoice}`;
+        compScore ++;
     }
 
     // Display the result
-    alert(result);
+    scoreDisplay.textContent = `User: ${userScore} | Computer: ${compScore}`;
+    resultDisplay.textContent = result;
+
+    if (userScore == winningScore || compScore == winningScore) {
+        displayWinner();
+    }
+}
+
+function resetGame() {
+    userScore = 0;
+    compScore = 0;
+
+    scoreDisplay.textContent = `User: ${userScore} | Computer: ${compScore}`;
+    resultDisplay.textContent ="";
+
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+
+    resetButton.style.display = 'none';
 }
 
 // Function to start the game (can be expanded if needed)
